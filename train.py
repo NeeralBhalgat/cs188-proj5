cd6ccd773db8a7443825cc2c56164dae9ccbe5a6
Local Commit <local@example.com>
2025-11-23 22:16:42 -0800

    Initial commit: solutions for Q1-Q2 with training adjustments

---
+++ train.py
@@ -0,0 +1,123 @@
+from torch import no_grad
+from torch.utils.data import DataLoader
+
+
+"""
+Functions you should use.
+Please avoid importing any other functions or modules.
+Your code will not pass if the gradescope autograder detects any changed imports
+"""
+from torch import optim, tensor
+from losses import regression_loss, digitclassifier_loss, languageid_loss, digitconvolution_Loss
+from torch import movedim
+
+
+"""
+##################
+### QUESTION 1 ###
+##################
+"""
+
+
+def train_perceptron(model, dataset):
+    """
+    Train the perceptron until convergence.
+    You can iterate through DataLoader in order to 
+    retrieve all the batches you need to train on.
+
+    Each sample in the dataloader is in the form {'x': features, 'label': label} where label
+    is the item we need to predict based off of its features.
+    """
+    with no_grad():
+        data = DataLoader(dataset, batch_size=1, shuffle=True)
+        "*** YOUR CODE HERE ***"
+        while True:
+            wrong = 0
+            for item in data:
+                x = item["x"]
+                label = item["label"].item()
+
+                if model(x) >= 0:
+                    guess = 1
+                else:
+                    guess = -1
+            
+                if guess != label:
+                    model.w += x.flatten() * label
+                    wrong += 1
+
+            if wrong == 0:
+                break
+
+
+def train_regression(model, dataset):
+    """
+    Trains the model.
+
+    In order to create batches, create a DataLoader object and pass in `dataset` as well as your required 
+    batch size. You can look at PerceptronModel as a guideline for how you should implement the DataLoader
+
+    Each sample in the dataloader object will be in the form {'x': features, 'label': label} where label
+    is the item we need to predict based off of its features.
+
+    Inputs:
+        model: Pytorch model to use
+        dataset: a PyTorch dataset object containing data to be trained on
+        
+    """
+       
+    "*** YOUR CODE HERE ***"
+    model.train()
+    batchsize = 16
+    data = DataLoader(dataset, batch_size=batchsize, shuffle=True)
+    lr = 0.001
+    opt = optim.Adam(model.parameters(), lr=0.001)
+
+    for _ in range(40000):
+        losses = []
+        for item in data:   
+            x = item["x"]
+            y = item["label"]
+
+            opt.zero_grad()
+            output = model(x)
+            loss = regression_loss(output, y)
+            loss.backward()
+            opt.step()
+
+            losses.append(loss.item())
+
+        mean = sum(losses) / len(losses)
+        if mean <= 0.02:
+            break
+
+
+def train_digitclassifier(model, dataset):
+    model.train()
+    """ YOUR CODE HERE """
+
+
+def train_languageid(model, dataset):
+    """
+    Trains the model.
+
+    Note that when you iterate through dataloader, each batch will returned as its own vector in the form
+    (batch_size x length of word x self.num_chars). However, in order to run multiple samples at the same time,
+    get_loss() and run() expect each batch to be in the form (length of word x batch_size x self.num_chars), meaning
+    that you need to switch the first two dimensions of every batch. This can be done with the movedim() function 
+    as follows:
+
+    movedim(input_vector, initial_dimension_position, final_dimension_position)
+
+    For more information, look at the pytorch documentation of torch.movedim()
+    """
+    model.train()
+    "*** YOUR CODE HERE ***"
+
+
+
+def Train_DigitConvolution(model, dataset):
+    """
+    Trains the model.
+    """
+    """ YOUR CODE HERE """
